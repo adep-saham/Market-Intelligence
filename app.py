40794f605856e0758a1bd4a4bc3ddc4cdaac41d3
adep-saham <best.adeprasetyo@gmail.com>
2025-11-28 22:28:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -249,8 +249,7 @@ elif menu == "Competitor":
     with col3:
         st.write("### UBS Gold")
         st.code(get_ubs_price())
-        st.code(ubs)
-
+       
         if ubs:
             st.metric("Harga Jual", f"Rp {ubs['jual']:,}")
             st.metric("Harga Beli", f"Rp {ubs['beli']:,}")
@@ -288,6 +287,7 @@ elif menu == "Pricing":
     st.metric("Harga Rekomendasi", f"Rp {recommended_price:,.0f}")
     st.markdown("### 📌 Gap Kompetitor")
     st.dataframe(gap.sort_values("gap"), use_container_width=True)
+
 
 
 
